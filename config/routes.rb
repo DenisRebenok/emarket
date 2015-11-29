a76f6f6ccaf8ed3aa17608a20f48400d6dba33f0
DenisRebenok <denis.rebenok.pro@gmail.com>
2015-11-29 15:24:54 +0200

    Made the first sale

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
 
-  get 'dashboard' => "pages#dashboard"
+  get 'dashboard' => 'pages#dashboard'
+
+  post '/buy/:slug', to: 'transactions#create', as: :buy
+  get '/pickup/:guid', to: 'transactions#pickup', as: :pickup
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
